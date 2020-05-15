03e984b3ed1f8ad5256fdfb4f37fa2c4747d00cd
DewhiteE <dewhiteeofficial@gmail.com>
2020-05-15 11:35:15 +0300

    Update stathelpers.py

--- stathelpers.py
+++ stathelpers.py
@@ -51,17 +51,33 @@ class Bernoulli:
     """
     def __init__(self, p, k, n):
         self.out_val = Bernoulli.get(p, k, n)
-        self.equation_str = Bernoulli.eqstr(p, k, n)
+        self.equation_str = Bernoulli.es(p, k, n)
     
     @staticmethod
     def get(p, k, n):
         return C(k, n) * pow(p, k) * pow((1-p), n-k)
     
     @staticmethod
-    def eqstr(p, k, n):
+    def es(p, k, n):
+        """
+        Equation string
+        """
         print("p =",p,"k =",k,"n =",n)
         equationstr = str()
         equationstr += "C("+str(k)+","+str(n)+")"+" * "+str(round(p,4))+"^"+str(k)+" * "
+        equationstr += "(1-"+str(round(p,4))+")^("+str(n)+"-"+str(k)+")"
+        equationstr += " = " + str(C(k,n))+" * "+str(round(pow(p, k),4))+" * "+str(round(pow((1-p), n-k),4))
+        equationstr += " = " + str(round(Bernoulli.get(p,k,n),4))
+        return equationstr
+    
+    @staticmethod
+    def wes(p, k, n):
+        """
+        Word document equation string
+        """
+        print("p =",p,"k =",k,"n =",n)
+        equationstr = str()
+        equationstr += "C^"+str(k)+"_"+str(n)+" * "+str(round(p,4))+"^"+str(k)+" * "
         equationstr += "(1-"+str(round(p,4))+")^("+str(n)+"-"+str(k)+")"
         equationstr += " = " + str(C(k,n))+" * "+str(round(pow(p, k),4))+" * "+str(round(pow((1-p), n-k),4))
         equationstr += " = " + str(round(Bernoulli.get(p,k,n),4))
@@ -77,7 +93,7 @@ class Bayes:
     def __init__(self, ah_list, h_list):
         self.out_val = Bayes.get(ah_list, h_list)
         self.full_probability = Bayes.get_ph(ah_list, h_list)
-        self.equation_str = Bayes.eqstr(ah_list, h_list)
+        self.equation_str = Bayes.es(ah_list, h_list)
     
     @staticmethod
     def get(ah_list, h_list):
@@ -118,18 +134,28 @@ class Bayes:
     #    return out
     
     @staticmethod
-    def eqstr(ah_list, h_list):
+    def es(ah_list, h_list):
         """
         ah_list -- list of probabilities of P(A|H) (can be any list of numerical values)
         h_list -- list of total probabilities of P(H)
         
         returns list of (H|A) probabilities
         """
-        print("Full probability =", Bayes.eqstr_ph(ah_list, h_list))
+        print("Full probability =", Bayes.es_ph(ah_list, h_list))
         for j in range(len(h_list)):
             print("P(B"+str(j)+"|A"+str(j)+") = (P(B"+str(j)+")*P(A"+str(j)+"|B"+str(j)+"))/P(A"+str(j)+") = (" + str(h_list[j]) + "*" + str(ah_list[j]) + ") / " + str(Bayes.get_ph(ah_list, h_list))
                   + " = " + str((h_list[j] * ah_list[j]) / Bayes.get_ph(ah_list, h_list)))
-    def eqstr_ph(ah_list, h_list):
+            
+    def wes(ah_list, h_list):
+        """
+        Word document equation string
+        """
+        Bayes.wes_ph(ah_list, h_list) # full probability
+        for j in range(len(h_list)):
+            print("P(B_"+str(j)+"|A_"+str(j)+") = (P(B_"+str(j)+")*P(A_"+str(j)+"|B_"+str(j)+"))/P(A_"+str(j)+") = ("+str(h_list[j])+"*"+str(ah_list[j])+")/"+str(Bayes.get_ph(ah_list, h_list))
+                  +" = "+str((round((h_list[j]*ah_list[j])/Bayes.get_ph(ah_list, h_list),4))))
+
+    def es_ph(ah_list, h_list):
         """
         ah_list -- list of probabilities of P(A|H) (can be any list of numerical values)
         h_list -- list of total probabilities of P(H)
@@ -142,13 +168,20 @@ class Bayes:
         equationstr = equationstr[:-3] + " = " + str(Bayes.get_ph(ah_list, h_list))
         return equationstr
     
+    def wes_ph(ah_list, h_list):
+        equationstr = str("P(A) = ")
+        for j in range(len(h_list)):
+            equationstr += "("+str(h_list[j])+"*"+str(ah_list[j])+")"+"+"
+        equationstr = equationstr[:-1] + " = " + str(Bayes.get_ph(ah_list, h_list))
+        print(equationstr)
+    
 class Moivre:
     """
     Use this method if np > 9 and n > 20
     """
     def __init__(self, p, k, n):
         self.out_val = Moivre.get(p, k, n)
-        self.equation_str = Moivre.eqstr(p, k, n)
+        self.equation_str = Moivre.es(p, k, n)
         #self.equation_str_integral = Mouivre.eqstr_integral(p, k1, k2, n)
         
     @staticmethod
@@ -162,8 +195,8 @@ class Moivre:
         return norm.cdf(Moivre.x(p,k2,n)) - norm.cdf(Moivre.x(p,k1,n))
     
     @staticmethod
-    def eqstr(p, k, n):
-        print("p =",p,"k =",k,"n =",n)
+    def es(p, k, n):
+        print("p =",p,", k =",k,", n =",n)
         equationstr = str()
         equationstr += "1 / " + "sqrt(" + str(n) + '*' + str(p) + '*(1-' + str(p) + '))'
         equationstr += " * φ(" + str(Moivre.x(p,k,n)) + ") = " + '1 / ' + str(math.sqrt(900*0.8*(1-0.8)))
@@ -172,16 +205,16 @@ class Moivre:
         return equationstr
     
     @staticmethod
-    def eqstr_x(p, k, n):
+    def es_x(p, k, n):
         equationstr = str()
         equationstr += 'x = (' + str(k) + '-' + str(n) + '*' + str(p)
         equationstr += ' / sqrt(' + str(n) + '*' + str(p) + '* (1-' + str(p) + '))'
         return equationstr
     
     @staticmethod
-    def eqstr_integral(p, k1, k2, n):
+    def es_integral(p, k1, k2, n):
         equationstr = str()
-        print('p =',p,'k1 =',k1,'k2 =',k2,'n =',n)
+        print('p =',p,', k1 =',k1,', k2 =',k2,', n =',n)
         print('x1 =',Moivre.eqstr_x(p, k1, n))
         print('x2 =',Moivre.eqstr_x(p, k2, n))
         equationstr += "Ф(x2) - Ф(x1) = " 
@@ -189,20 +222,31 @@ class Moivre:
         equationstr += str(norm.cdf(Moivre.x(p,k2,n))) + " - " + str(norm.cdf(Moivre.x(p,k1,n)))
         equationstr += " = " + str(Moivre.integral(p,k1,k2,n))
         return equationstr
+    
+    @staticmethod
+    def wes(p, k, n):
+        print("p =",p,", k =",k,", n =",n)
+        equationstr = "P{k=" + str(k) + "} = 1/\sqrt(np(1-p))*φ(x) = "
+        equationstr += "1/"+"\sqrt("+str(n)+'*'+str(p)+'*(1-'+str(p)+'))'
+        equationstr += "*φ("+str(round(Moivre.x(p,k,n),5))+")="+'1/'+str(round(math.sqrt(900*0.8*(1-0.8)),5))
+        equationstr += '*'+str(round(phi(Moivre.x(p, k, n)),5))
+        equationstr += ' = ' + str(round(Moivre.get(p, k, n),5))
+        print(equationstr)
+    
 class Poisson:
     """
     Use this method if np < 9 and n > 20
     """
     def __init__(self, p, k, n):
         self.out_val = Poisson.get(p, k, n)
-        self.equation_str = Poisson.eqstr(p, k, n)
+        self.equation_str = Poisson.es(p, k, n)
     
     @staticmethod
     def get(p, k ,n):
         return ((pow((n*p),k) / math.factorial(k)) * pow(math.e, -n * p))
     
     @staticmethod
-    def eqstr(p, k, n):
+    def es(p, k, n):
         equationstr = str()
         print('P =', p, ', k =', k, ', n =', n)
         equationstr += str('(' + str(n) + '*' + str(p) + ')^2 ')
@@ -215,7 +259,7 @@ class RandomVariation:
     """
     def __init__(self, arr):
         self.out_val = RandomVariation.get(arr)
-        self.equation_str = RandomVariation.eqstr(arr)
+        self.equation_str = RandomVariation.es(arr)
     
     @staticmethod
     def get(arr):
@@ -234,7 +278,7 @@ class RandomVariation:
         return integrate.quad(lambda x: pow(x-mx, 2)*func(x), a, b)
     
     @staticmethod
-    def eqstr(arr):
+    def es(arr):
         """
         arr -- must be an array of pairs, where first argument (key) is xi
         and second (value) is P(X=xi) 
@@ -260,14 +304,14 @@ class StandardDeviation:
     """
     def __init__(self, arr):
         self.out_val = StandardDeviation.get(arr)
-        self.equation_str = StandardDeviation.eqstr(arr)
+        self.equation_str = StandardDeviation.es(arr)
     
     @staticmethod
     def get(arr):
         return math.sqrt(RandomVariation.get(arr))
     
     @staticmethod
-    def eqstr(arr):
+    def es(arr):
         return str("σ(x) = sqrt(D(X)) = sqrt(" + str(RandomVariation.get(arr)) + ") = " + str(math.sqrt(RandomVariation.get(arr))))
 
 class MathExpectation:
@@ -275,7 +319,7 @@ class MathExpectation:
     """  
     def __init__(self, arr):
         self.out_val = MathExpectation.get(arr)
-        self.equation_str = MathExpectation.eqstr(arr)
+        self.equation_str = MathExpectation.es(arr)
     
     @staticmethod
     def get(arr):
@@ -292,7 +336,7 @@ class MathExpectation:
         return integrate.quad(lambda x: x * func(x), a, b)
     
     @staticmethod
-    def eqstr(arr):
+    def es(arr):
         """
         arr -- must be an array of pairs, where first argument (key) is xi
         and second (value) is P(X=xi) 
